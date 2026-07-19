1e21a9fb22127426cb93102224209db70aca40a6
contrerasrdianac406 <contrerasrdianac@gmail.com>
2026-07-19 11:36:11 -0500

    Feat[Views]: se crean los views para home, chat, about y noFound

---
+++ src/views/home.js
@@ -0,0 +1,6 @@
+export function renderHome() 
+{ const app = document.getElementById('app'); app.innerHTML = 
+    ` <section> 
+    <h2>Bienvenido a Chat Tropicoqueta 👋</h2> 
+    <p>La plataforma de chat que te permite hablar con Karol G.</p> 
+    <p>Navegá a <strong>Chat</strong> para ver tus conversaciones, sin perder nada en el camino.</p> </section> `; }
